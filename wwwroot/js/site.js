44317d6c5e7911cb09897c8398933cdb3ad5b202
Suresh Kalaga <suresh.kalaga@outlook.com>
2024-01-07 22:11:37 +0530

    wwwroot added.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,46 @@
+﻿function fnLoadImage() {
+    var path = window.location.href;
+	$('.nav-item').each(function () {
+		var item = this;
+		$(item).find('a').removeClass('btn btn-primary');
+		$(item).find('a').addClass('btn btn-secondary');
+		$(item).find('a').css('color', 'white');
+    });
+    $('.nav-item').each(function () {
+        var item = this;
+        var val = $(item).find('a').attr('href');
+		val = val.replace('./','');
+        if (path.indexOf(val) != -1) {
+			$(item).find('a').removeClass('btn btn-secondary');
+            $(item).find('a').addClass('btn btn-primary');
+			return;
+        }
+    });
+}
+
+let current = -1;
+function showNextImage() {
+    var len = model.length;
+    for (var i = 0; i < len; i++) {
+        var name = $('#zoomImg').prop('src');
+        var namesrc = decodeURI(name);
+        var src = model[i].imageURL;
+		var fnsrc = namesrc.substring(namesrc.lastIndexOf('/')+1);
+		var fsrc = src.substring(src.lastIndexOf('/')+1);
+		
+        if (fnsrc.indexOf(fsrc) != -1) {
+            current = i;
+            var isrc = getNextImage(len);
+            $('#zoomImg').prop('src', isrc);
+            return;
+        }
+    }
+}
+
+function getNextImage(len) {
+    if (current + 1 > len - 1) {
+        current = 0;
+    }
+    current = current + 1;
+    return model[current].imageURL;
+}
